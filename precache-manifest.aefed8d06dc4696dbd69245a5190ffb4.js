f062ba219d84b482276a030fda0d1d325f12a335
Roman Kruglyanskiy <eviltroll@yandex.ru>
2021-02-09 15:10:00 +0300

    Updates

---
+++ precache-manifest.aefed8d06dc4696dbd69245a5190ffb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62f846e8d523299021389a923ccd2b6",
+    "url": "/testYourBrainApp/index.html"
+  },
+  {
+    "revision": "c9b0e51e3e4470b3316c",
+    "url": "/testYourBrainApp/static/css/main.140b143c.chunk.css"
+  },
+  {
+    "revision": "befd935bd1e1f20cd4b9",
+    "url": "/testYourBrainApp/static/js/2.db222eb1.chunk.js"
+  },
+  {
+    "revision": "9db4efcfaceec51311f18228d56f3007",
+    "url": "/testYourBrainApp/static/js/2.db222eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b0e51e3e4470b3316c",
+    "url": "/testYourBrainApp/static/js/main.23742926.chunk.js"
+  },
+  {
+    "revision": "5e42edb1c77e01d0c32b",
+    "url": "/testYourBrainApp/static/js/runtime-main.9b163591.js"
+  }
+]);
--- src/axios/axios-quiz.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-    baseURL: 'https://test-ce86f-default-rtdb.europe-west1.firebasedatabase.app/'
-    // baseURL: 'https://react-quiz-fa011.firebaseio.com/'
-})
