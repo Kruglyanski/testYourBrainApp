d54c248c91e23575e47ecca56c92a6090e2920a7
Roman Kruglyanskiy <eviltroll@yandex.ru>
2021-07-05 14:31:36 +0300

    fix: firebase baseURL

--- src/axios/axios-quiz.js
+++ src/axios/axios-quiz.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://test-ce86f-default-rtdb.europe-west1.firebasedatabase.app/'
+    baseURL: 'https://quiz-48fc2-default-rtdb.europe-west1.firebasedatabase.app/'
     // baseURL: 'https://react-quiz-fa011.firebaseio.com/'
 })
